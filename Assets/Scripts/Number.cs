b667908bc04bf1057519bdb8db5856a5327ce00b
TheLukCraft <luk1997r@o2.pl>
2020-09-19 17:32:27 +0200

    Game object number

--- Assets/Scripts/Number.cs
+++ Assets/Scripts/Number.cs
@@ -1,18 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Number : MonoBehaviour
 {
+    [SerializeField]
+    Sprite[] Sprites;
+
+    private int _value = 0;
+    public int Value
+    {
+        get { return _value; }
+        set
+        {
+            _value = Value;
+            RefreshNumbers();
+        }
+    }
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Value = 123;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RefreshNumbers()
     {
-        
+        var digits = Value
+            .ToString()
+            .Select(c => int.Parse(c.ToString()))
+            .ToArray();
+
+        for(int i=0; i<digits.Count(); i++)
+        {
+            var position = GetPosition(i);
+            CreateDigit(position, Sprites[i]);
+        }
+    }
+
+    private Vector3 GetPosition(int i)
+    {
+        return Vector3.right * i;
+    }
+
+    private void CreateDigit(Vector3 position, Sprite sprite)
+    {
+        var digit = new GameObject();
+        digit.transform.parent = transform;
+        digit.transform.localPosition = position;
+
+        digit.AddComponent<SpriteRenderer>().sprite = sprite;
     }
 }
