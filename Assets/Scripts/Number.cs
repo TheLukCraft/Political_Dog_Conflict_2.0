0cca0fcaff1122e335c86c169a46fda0ac067bac
TheLukCraft <luk1997r@o2.pl>
2020-09-19 17:52:41 +0200

    Bug fixing numbers

--- Assets/Scripts/Number.cs
+++ Assets/Scripts/Number.cs
@@ -4,10 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+public enum TextDirection { Left, Middle, Right }
+
 public class Number : MonoBehaviour
 {
     [SerializeField]
     Sprite[] Sprites;
+
+    [SerializeField]
+    float GridSize = 1f;
+
+    [SerializeField]
+    TextDirection TextDirection;
 
     private int _value = 0;
     public int Value
@@ -24,7 +32,7 @@ public class Number : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Value = 123;
+        Value = 432343;
     }
 
     private void RefreshNumbers()
@@ -36,22 +44,32 @@ public class Number : MonoBehaviour
 
         for(int i=0; i<digits.Count(); i++)
         {
-            var position = GetPosition(i);
-            CreateDigit(position, Sprites[i]);
+            var position = CalculatePosition(i, digits.Count());
+            CreateDigit(position, digits[i]);
         }
     }
 
-    private Vector3 GetPosition(int i)
+    private Vector3 CalculatePosition(int index, int numberOfDigits)
     {
-        return Vector3.right * i;
+        float result = 0;
+
+       if(TextDirection == TextDirection.Left)
+            result = index;
+       else if(TextDirection == TextDirection.Middle)
+            result = index - numberOfDigits / 2f + 0.5f;
+       else if(TextDirection == TextDirection.Right)
+            result = index - numberOfDigits + 1;
+
+        return Vector3.right * result * GridSize;
     }
 
-    private void CreateDigit(Vector3 position, Sprite sprite)
+    private void CreateDigit(Vector3 position, int value)
     {
         var digit = new GameObject();
         digit.transform.parent = transform;
         digit.transform.localPosition = position;
 
+        var sprite = Sprites[value];
         digit.AddComponent<SpriteRenderer>().sprite = sprite;
     }
 }
